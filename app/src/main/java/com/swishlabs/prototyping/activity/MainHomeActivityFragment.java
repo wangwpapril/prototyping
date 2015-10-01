e9852590181d73340001c1dacb9c5a1eb79ce4bb
Will Wang <wwang@peakcontact.com>
2015-10-01 11:18:38 -0400

    recyclerview with swip dismiss and reorder

--- app/src/main/java/com/swishlabs/prototyping/activity/MainHomeActivityFragment.java
+++ app/src/main/java/com/swishlabs/prototyping/activity/MainHomeActivityFragment.java
@@ -2,18 +2,27 @@ package com.swishlabs.prototyping.activity;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.swishlabs.prototyping.R;
+import com.swishlabs.prototyping.adapter.MainHomeListAdapter;
 import com.swishlabs.prototyping.helper.OnStartDragListener;
+import com.swishlabs.prototyping.helper.SimpleItemTouchHelperCallback;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainHomeActivityFragment extends Fragment implements OnStartDragListener {
+
+    private RecyclerView mRecyclerView;
+//    private RecyclerView.Adapter<CustomViewHolder> mAdapter;
+    private ItemTouchHelper mItemTouchHelper;
+
 
     public MainHomeActivityFragment() {
     }
@@ -25,7 +34,25 @@ public class MainHomeActivityFragment extends Fragment implements OnStartDragLis
     }
 
     @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        MainHomeListAdapter adapter = new MainHomeListAdapter(getActivity(), this);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(adapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(adapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+
+    }
+
+    @Override
     public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
 
     }
 }
