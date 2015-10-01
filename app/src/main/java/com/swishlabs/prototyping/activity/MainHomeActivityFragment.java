24e0b3c1938ad2d64dbf106e3bee5e4c75096823
Will Wang <wwang@peakcontact.com>
2015-10-01 09:52:22 -0400

    ItemTouch Helper

--- app/src/main/java/com/swishlabs/prototyping/activity/MainHomeActivityFragment.java
+++ app/src/main/java/com/swishlabs/prototyping/activity/MainHomeActivityFragment.java
@@ -2,16 +2,18 @@ package com.swishlabs.prototyping.activity;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.swishlabs.prototyping.R;
+import com.swishlabs.prototyping.helper.OnStartDragListener;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainHomeActivityFragment extends Fragment {
+public class MainHomeActivityFragment extends Fragment implements OnStartDragListener {
 
     public MainHomeActivityFragment() {
     }
@@ -20,5 +22,10 @@ public class MainHomeActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main_home, container, false);
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+
     }
 }
